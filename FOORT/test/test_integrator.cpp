3470b36b5ff9ba2821afc4625336548f25c51463
Seppe Staelens <seppe.staelens@hotmail.com>
2024-12-12 12:57:59 +0000

    bound geodesic test complete

--- FOORT/test/test_integrator.cpp
+++ FOORT/test/test_integrator.cpp
@@ -33,7 +33,7 @@ TEST(Integrators, rk4_bound_geodesic)
     BoundarySphereTermination::TermOptions =
         std::unique_ptr<BoundarySphereTermOptions>(new BoundarySphereTermOptions{10, false, 1});
     TimeOutTermination::TermOptions =
-        std::unique_ptr<TimeOutTermOptions>(new TimeOutTermOptions{500, 1});
+        std::unique_ptr<TimeOutTermOptions>(new TimeOutTermOptions{10000, 1});
 
     GeodesicIntegratorFunc theIntegrator = Integrators::IntegrateGeodesicStep_RK4;
     Integrators::IntegratorDescription = "RK4";
@@ -43,7 +43,7 @@ TEST(Integrators, rk4_bound_geodesic)
                      theIntegrator);
 
     // Set up initial conditions for the bound geodesic
-    Point initpos = {0.0, 3.0 - 1.e-10, pi / 2.0, 0.0};
+    Point initpos = {0.0, 3.0, pi / 2.0, 0.0};
 
     TwoIndex gdd = theM->getMetric_dd(initpos);
     OneIndex initvel = {-1.0 / sqrt(-gdd[0][0]), 0.0, 0.0, 1.0 / sqrt(gdd[3][3])};
@@ -51,18 +51,21 @@ TEST(Integrators, rk4_bound_geodesic)
 
     theGeod.Reset(scrindex, initpos, initvel);
 
-    Point current_pos;
-    double current_phi;
+    Point current_pos{initpos};
+    double current_phi{0.0}, current_r{3.0};
+    std::ofstream file("rk4_bound_geodesic.dat");
     while (theGeod.getTermCondition() == Term::Continue && current_phi < 5 * pi)
     {
         theGeod.Update();
         current_pos = theGeod.getCurrentPos();
+        current_r = current_pos[1];
         current_phi = current_pos[3];
+        file << std::setprecision(15) << current_r << " " << current_phi << std::endl;
     }
-
+    file.close();
     double final_r = current_pos[1];
     double final_theta = current_pos[2];
-    EXPECT_NEAR(final_r, 3.0, 0.001) << "Final radial position too large after 5 half-orbits: " << final_r << std::endl;
+    EXPECT_NEAR(final_r, 3.0, 0.003) << "Final radial position too large after 5 half-orbits: " << final_r << std::endl;
     EXPECT_NEAR(final_theta, pi / 2.0, 0.000000001) << "Final theta position not close enough to pi/2 after 5 half-orbits: " << final_theta << std::endl;
 }
 
@@ -88,7 +91,7 @@ TEST(Integrators, verlet_bound_geodesic)
     BoundarySphereTermination::TermOptions =
         std::unique_ptr<BoundarySphereTermOptions>(new BoundarySphereTermOptions{10, false, 1});
     TimeOutTermination::TermOptions =
-        std::unique_ptr<TimeOutTermOptions>(new TimeOutTermOptions{500, 1});
+        std::unique_ptr<TimeOutTermOptions>(new TimeOutTermOptions{10000, 1});
 
     GeodesicIntegratorFunc theIntegrator = Integrators::IntegrateGeodesicStep_Verlet;
     Integrators::IntegratorDescription = "Verlet";
@@ -98,7 +101,7 @@ TEST(Integrators, verlet_bound_geodesic)
                      theIntegrator);
 
     // Set up initial conditions for the bound geodesic
-    Point initpos = {0.0, 3.0 + 1e-16, pi / 2.0, 0.0};
+    Point initpos = {0.0, 3.0, pi / 2.0, 0.0};
 
     TwoIndex gdd = theM->getMetric_dd(initpos);
     OneIndex initvel = {-1.0 / sqrt(-gdd[0][0]), 0.0, 0.0, 1.0 / sqrt(gdd[3][3])};
@@ -106,18 +109,20 @@ TEST(Integrators, verlet_bound_geodesic)
 
     theGeod.Reset(scrindex, initpos, initvel);
 
-    Point current_pos;
-    double current_phi;
+    Point current_pos{initpos};
+    double current_phi{0.0}, current_r{3.0};
+    std::ofstream file("verlet_bound_geodesic.dat");
     while (theGeod.getTermCondition() == Term::Continue && current_phi < 5 * pi)
     {
         theGeod.Update();
         current_pos = theGeod.getCurrentPos();
+        current_r = current_pos[1];
         current_phi = current_pos[3];
-        std::cout << "Current radius: " << current_pos[1] << std::endl;
+        file << std::setprecision(15) << current_r << " " << current_phi << std::endl;
     }
-
+    file.close();
     double final_r = current_pos[1];
     double final_theta = current_pos[2];
-    EXPECT_NEAR(final_r, 3.0, 0.001) << "Final radial position too large after 5 half-orbits: " << final_r << std::endl;
+    EXPECT_NEAR(final_r, 3.0, 0.003) << "Final radial position too large after 5 half-orbits: " << final_r << std::endl;
     EXPECT_NEAR(final_theta, pi / 2.0, 0.000000001) << "Final theta position not close enough to pi/2 after 5 half-orbits: " << final_theta << std::endl;
 }
